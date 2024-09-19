35eedfa7ea804fbd09f9fdc14a46fb33e568397b
DevRafaelLuz <rafael.luz2005@gmail.com>
2024-09-19 15:00:08 -0300

    Implementando as classes da pasta Models

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("------------------------------------------");
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia(numero: "(99) 99999-9999", modelo: "ModeloX", imei: "00000000", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("------------------------------------------");
+
+Console.WriteLine("Smartphone iPhone");
+Iphone iphone = new Iphone(numero: "(11) 11111-1111", modelo: "ModeloY", imei: "11111111", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+Console.WriteLine("------------------------------------------");
